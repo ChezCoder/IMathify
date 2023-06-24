eb929d6639dee43cbe58acebe2e6f05e027175bd
ChezCoder <62308406+ChezCoder@users.noreply.github.com>
2023-06-24 14:31:12 -0400

    Page loading screen + sync dynamic script loading

--- static/common.js
+++ static/common.js
@@ -2,13 +2,25 @@
 window.addEventListener("load", function() {
     const scripts = [
         // jQuery
-        { "type": "script", "src": "/static/lib/jquery/jquery.min.js" },
+        { "type": "js", "src": "/static/lib/jquery/jquery.min.js" },
 
         // mdb.js
-        { "type": "script", "src": "/static/lib/mdb/js/mdb.min.js" },
+        { "type": "js", "src": "/static/lib/mdb/js/mdb.min.js" },
 
         // mdb.css
-        { "type": "link", "src": "/static/lib/mdb/css/mdb.min.css" },
+        { "type": "css", "src": "/static/lib/mdb/css/mdb.min.css" },
+
+        // dark theme
+        { "type": "css", "src": "/static/dark.css" },
+
+        // light theme
+        { "type": "css", "src": "/static/light.css" },
+
+        // main script.js
+        { "type": "js", "src": "./script.js" },
+
+        // common css
+        { "type": "css", "src": "/static/common.css" },
     ];
 
     const LOG_STYLES = [
@@ -18,27 +30,30 @@ window.addEventListener("load", function() {
 
     console.log("%cDynamic Scripts Loader", "color: #ff0000;font-size: 15px;padding: 5px;");
     
-    scripts.forEach(function(definition) {
-        let element;
-
-        // configure element to their respective attributes
-        switch (definition.type) {
-            case "script": {
-                element = document.createElement("script");
-                element.src = definition.src;
-                console.log(`%cLoaded JavaScript: %c${definition.src}`, ...LOG_STYLES);
-                break;
+    (async function() {
+        for (const script of scripts) {
+            let element;
+        
+            // configure element to their respective attributes
+            switch (script.type) {
+                case "js": {
+                    element = document.createElement("script");
+                    element.src = script.src;
+                    console.log(`%cLoaded JavaScript: %c${script.src}`, ...LOG_STYLES);
+                    break;
+                }
+                case "css": {
+                    element = document.createElement("link");
+                    element.rel = "stylesheet";
+                    element.href = script.src;
+                    console.log(`%cLoaded StyleSheet: %c${script.src}`, ...LOG_STYLES);
+                    break;
+                }
             }
-            case "link": {
-                element = document.createElement("link");
-                element.rel = "stylesheet";
-                element.href = definition.src;
-                console.log(`%cLoaded StyleSheet: %c${definition.src}`, ...LOG_STYLES);
-                break;
-            }
+        
+            // load elements sync
+            document.head.appendChild(element);
+            await new Promise(r => element.addEventListener("load", r));
         }
-
-        // load element
-        document.head.appendChild(element);
-    });
+    })();
 });
