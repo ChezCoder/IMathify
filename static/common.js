5712b4537ea6fda40122b4bc6c05042f633cbee6
ChezCoder <62308406+ChezCoder@users.noreply.github.com>
2023-06-24 14:02:38 -0400

    Fixed dynamic scripts loader

--- static/common.js
+++ static/common.js
@@ -10,24 +10,35 @@ window.addEventListener("load", function() {
         // mdb.css
         { "type": "link", "src": "/static/lib/mdb/css/mdb.min.css" },
     ];
+
+    const LOG_STYLES = [
+        "color: #00ffff;padding: 10px;",
+        "color: 9999ff; text-decoration: underline;"
+    ];
+
+    console.log("%cDynamic Scripts Loader", "color: #ff0000;font-size: 15px;padding: 5px;");
     
     scripts.forEach(function(definition) {
         let element;
 
         // configure element to their respective attributes
         switch (definition.type) {
-            case "script":
+            case "script": {
                 element = document.createElement("script");
                 element.src = definition.src;
+                console.log(`%cLoaded JavaScript: %c${definition.src}`, ...LOG_STYLES);
                 break;
-            case "link":
+            }
+            case "link": {
                 element = document.createElement("link");
                 element.rel = "stylesheet";
                 element.href = definition.src;
+                console.log(`%cLoaded StyleSheet: %c${definition.src}`, ...LOG_STYLES);
                 break;
+            }
         }
 
         // load element
-        document.appendChild(element);
+        document.head.appendChild(element);
     });
 });
